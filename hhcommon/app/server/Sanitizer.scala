80bc9819a0055dab8909c335212db9ad9c91b1c0
Xavier Tordoir <xtordoir@yahoo.co.uk>
2015-03-24 00:58:31 +0100

    indentation cleanup

--- hhcommon/app/server/Sanitizer.scala
+++ hhcommon/app/server/Sanitizer.scala
@@ -4,29 +4,29 @@ import play.api.libs.json._
 
 trait Sanitizer {
 
-	// common...can do better ;)
-	def unionField(j: JsValue, utype: String): JsValue = j match {
-		case _: JsUndefined => JsNull
-		case x: JsValue  => Json.obj(utype -> x)
-	}
+  // common...can do better ;)
+  def unionField(j: JsValue, utype: String): JsValue = j match {
+    case _: JsUndefined => JsNull
+    case x: JsValue  => Json.obj(utype -> x)
+  }
 
 
-	// reccusrive function to add-up missing fields
-	def sanField(fields: List[(String, () => JsObject, JsValue => JsObject)], json: JsObject): JsObject = fields match {
-		case head :: tail => {
-			val tmp = json \ head._1 match {
-				case _: JsUndefined => head._2()
-				case x: JsValue     => head._3(x)
-			}
-			val njson = json ++ tmp
-			sanField(tail, njson)
-		}
-		case Nil => json
-	}
+  // reccusrive function to add-up missing fields
+  def sanField(fields: List[(String, () => JsObject, JsValue => JsObject)], json: JsObject): JsObject = fields match {
+    case head :: tail => {
+      val tmp = json \ head._1 match {
+        case _: JsUndefined => head._2()
+        case x: JsValue     => head._3(x)
+      }
+      val njson = json ++ tmp
+      sanField(tail, njson)
+    }
+    case Nil => json
+  }
 
-	def sanitize(jsonTxt: String, fields: List[(String, () => JsObject, (JsValue) => JsObject)]): String = {
-		val json: JsValue = Json.parse(jsonTxt)
-		val sanJson = sanField(fields, json.as[JsObject])
-		sanJson.as[JsValue].toString()
-	}	
+  def sanitize(jsonTxt: String, fields: List[(String, () => JsObject, (JsValue) => JsObject)]): String = {
+    val json: JsValue = Json.parse(jsonTxt)
+    val sanJson = sanField(fields, json.as[JsObject])
+    sanJson.as[JsValue].toString()
+  } 
 }
