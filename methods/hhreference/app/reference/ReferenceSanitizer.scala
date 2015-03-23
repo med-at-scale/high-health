de6f7740b2a1d93b512c94f22c0db754207c9a05
Xavier Tordoir <xtordoir@yahoo.co.uk>
2015-03-24 00:56:07 +0100

    indentation cleanup

--- methods/hhreference/app/reference/ReferenceSanitizer.scala
+++ methods/hhreference/app/reference/ReferenceSanitizer.scala
@@ -4,50 +4,50 @@ import play.api.libs.json._
 
 object ReferenceSanitizer {
 
-	// common...can do better ;)
-	def unionField(j: JsValue, utype: String): JsValue = j match {
-		case _: JsUndefined => JsNull
-		case x: JsValue  => Json.obj(utype -> x)
-	}
+  // common...can do better ;)
+  def unionField(j: JsValue, utype: String): JsValue = j match {
+    case _: JsUndefined => JsNull
+    case x: JsValue  => Json.obj(utype -> x)
+  }
 
 
-	// reccusrive function to add-up missing fields
-	def sanField(fields: List[(String, () => JsObject, JsValue => JsObject)], json: JsObject): JsObject = fields match {
-		case head :: tail => {
-			val tmp = json \ head._1 match {
-				case _: JsUndefined => head._2()
-				case x: JsValue     => head._3(x)
-			}
-			val njson = json ++ tmp
-			sanField(tail, njson)
-		}
-		case Nil => json
-	}
+  // reccusrive function to add-up missing fields
+  def sanField(fields: List[(String, () => JsObject, JsValue => JsObject)], json: JsObject): JsObject = fields match {
+    case head :: tail => {
+      val tmp = json \ head._1 match {
+        case _: JsUndefined => head._2()
+        case x: JsValue     => head._3(x)
+      }
+      val njson = json ++ tmp
+      sanField(tail, njson)
+    }
+    case Nil => json
+  }
 
-	def searchReferenceSetsRequest(jsonTxt: String): String = {
-		val json: JsValue = Json.parse(jsonTxt)
-		val fields = List(
-			("md5checksums", () => Json.obj("md5checksums" -> Json.toJson(List[String]())), (j: JsValue) => j.as[JsObject]),
-			("assemblyId",   () => Json.obj("assemblyId" -> JsNull), (j: JsValue) => Json.obj("assemblyId" -> j)),
-			("pageSize",     () => Json.obj("pageSize" -> JsNull),   (j: JsValue) => Json.obj("pageSize"  -> unionField(j, "int"))),
-			("pageToken",    () => Json.obj("pageToken" -> JsNull),  (j: JsValue) => Json.obj("pageToken" -> unionField(j, "string")))
-			)
-		val sanJson = sanField(fields, json.as[JsObject])
-		sanJson.as[JsValue].toString()
-	}
+  def searchReferenceSetsRequest(jsonTxt: String): String = {
+    val json: JsValue = Json.parse(jsonTxt)
+    val fields = List(
+      ("md5checksums", () => Json.obj("md5checksums" -> Json.toJson(List[String]())), (j: JsValue) => j.as[JsObject]),
+      ("assemblyId",   () => Json.obj("assemblyId" -> JsNull), (j: JsValue) => Json.obj("assemblyId" -> j)),
+      ("pageSize",     () => Json.obj("pageSize" -> JsNull),   (j: JsValue) => Json.obj("pageSize"  -> unionField(j, "int"))),
+      ("pageToken",    () => Json.obj("pageToken" -> JsNull),  (j: JsValue) => Json.obj("pageToken" -> unionField(j, "string")))
+      )
+    val sanJson = sanField(fields, json.as[JsObject])
+    sanJson.as[JsValue].toString()
+  }
 
-	def searchReferencesRequest(jsonTxt: String): String = {
-		val json: JsValue = Json.parse(jsonTxt)
-		val fields = List(
-			("referenceSetId", () => Json.obj("referenceSetId" -> JsNull), (j: JsValue) => Json.obj("referenceSetId" -> j)),
-			("parentReferenceIds", () => Json.obj("parentReferenceIds" -> Json.toJson(List[String]())), (j: JsValue) => j.as[JsObject]),
-			("accessions", () => Json.obj("accessions" -> Json.toJson(List[String]())), (j: JsValue) => j.as[JsObject]),
-			("referenceNames", () => Json.obj("referenceNames" -> Json.toJson(List[String]())), (j: JsValue) => j.as[JsObject]),
-			("pageSize",     () => Json.obj("pageSize" -> JsNull),   (j: JsValue) => Json.obj("pageSize"  -> unionField(j, "int"))),
-			("pageToken",    () => Json.obj("pageToken" -> JsNull),  (j: JsValue) => Json.obj("pageToken" -> unionField(j, "string")))
-			)
-		val sanJson = sanField(fields, json.as[JsObject])
-		sanJson.as[JsValue].toString()
-	}
+  def searchReferencesRequest(jsonTxt: String): String = {
+    val json: JsValue = Json.parse(jsonTxt)
+    val fields = List(
+      ("referenceSetId", () => Json.obj("referenceSetId" -> JsNull), (j: JsValue) => Json.obj("referenceSetId" -> j)),
+      ("parentReferenceIds", () => Json.obj("parentReferenceIds" -> Json.toJson(List[String]())), (j: JsValue) => j.as[JsObject]),
+      ("accessions", () => Json.obj("accessions" -> Json.toJson(List[String]())), (j: JsValue) => j.as[JsObject]),
+      ("referenceNames", () => Json.obj("referenceNames" -> Json.toJson(List[String]())), (j: JsValue) => j.as[JsObject]),
+      ("pageSize",     () => Json.obj("pageSize" -> JsNull),   (j: JsValue) => Json.obj("pageSize"  -> unionField(j, "int"))),
+      ("pageToken",    () => Json.obj("pageToken" -> JsNull),  (j: JsValue) => Json.obj("pageToken" -> unionField(j, "string")))
+      )
+    val sanJson = sanField(fields, json.as[JsObject])
+    sanJson.as[JsValue].toString()
+  }
 
 }
--- methods/hhvariant/app/variant/VariantSanitizer.scala
+++ methods/hhvariant/app/variant/VariantSanitizer.scala
@@ -6,41 +6,41 @@ import server.Sanitizer
 object VariantSanitizer extends Sanitizer {
 
     def searchVariantsRequest(jsonTxt: String): String = {
-		val json: JsValue = Json.parse(jsonTxt)
-		val fields = List(
-			("pageSize",     () => Json.obj("pageSize" -> JsNull),   (j: JsValue) => Json.obj("pageSize"  -> unionField(j, "int"))),
-			("pageToken",    () => Json.obj("pageToken" -> JsNull),  (j: JsValue) => Json.obj("pageToken" -> unionField(j, "string"))),
-			("referenceId",  () => Json.obj("referenceId" -> JsNull),(j: JsValue) => Json.obj("referenceId" -> unionField(j, "string"))),
-			("referenceName",  () => Json.obj("referenceName" -> JsNull),(j: JsValue) => Json.obj("referenceName" -> unionField(j, "string"))),
-			("variantName",  () => Json.obj("variantName" -> JsNull),(j: JsValue) => Json.obj("variantName" -> unionField(j, "string"))),
-			("callSetIds", () => Json.obj("callSetIds" -> JsNull), (j: JsValue) => j.as[JsObject])
-		)
+    val json: JsValue = Json.parse(jsonTxt)
+    val fields = List(
+      ("pageSize",     () => Json.obj("pageSize" -> JsNull),   (j: JsValue) => Json.obj("pageSize"  -> unionField(j, "int"))),
+      ("pageToken",    () => Json.obj("pageToken" -> JsNull),  (j: JsValue) => Json.obj("pageToken" -> unionField(j, "string"))),
+      ("referenceId",  () => Json.obj("referenceId" -> JsNull),(j: JsValue) => Json.obj("referenceId" -> unionField(j, "string"))),
+      ("referenceName",  () => Json.obj("referenceName" -> JsNull),(j: JsValue) => Json.obj("referenceName" -> unionField(j, "string"))),
+      ("variantName",  () => Json.obj("variantName" -> JsNull),(j: JsValue) => Json.obj("variantName" -> unionField(j, "string"))),
+      ("callSetIds", () => Json.obj("callSetIds" -> JsNull), (j: JsValue) => j.as[JsObject])
+    )
 
-		val sanJson = sanField(fields, json.as[JsObject])
-		sanJson.as[JsValue].toString()
-	}
+    val sanJson = sanField(fields, json.as[JsObject])
+    sanJson.as[JsValue].toString()
+  }
 
-	def searchVariantSetsRequest(jsonTxt: String): String = {
-		val json: JsValue = Json.parse(jsonTxt)
-		val fields = List(
-			("pageSize",     () => Json.obj("pageSize" -> JsNull),   (j: JsValue) => Json.obj("pageSize"  -> unionField(j, "int"))),
-			("pageToken",    () => Json.obj("pageToken" -> JsNull),  (j: JsValue) => Json.obj("pageToken" -> unionField(j, "string")))
-		)
+  def searchVariantSetsRequest(jsonTxt: String): String = {
+    val json: JsValue = Json.parse(jsonTxt)
+    val fields = List(
+      ("pageSize",     () => Json.obj("pageSize" -> JsNull),   (j: JsValue) => Json.obj("pageSize"  -> unionField(j, "int"))),
+      ("pageToken",    () => Json.obj("pageToken" -> JsNull),  (j: JsValue) => Json.obj("pageToken" -> unionField(j, "string")))
+    )
 
-		val sanJson = sanField(fields, json.as[JsObject])
-		sanJson.as[JsValue].toString()		
-	}
+    val sanJson = sanField(fields, json.as[JsObject])
+    sanJson.as[JsValue].toString()    
+  }
 
-	def searchCallSetsRequest(jsonTxt: String): String = {
-		val json: JsValue = Json.parse(jsonTxt)
-		val fields = List(
-			("name",     () => Json.obj("name" -> JsNull),   (j: JsValue) => Json.obj("name"  -> unionField(j, "string"))),
-			("pageSize",     () => Json.obj("pageSize" -> JsNull),   (j: JsValue) => Json.obj("pageSize"  -> unionField(j, "int"))),
-			("pageToken",    () => Json.obj("pageToken" -> JsNull),  (j: JsValue) => Json.obj("pageToken" -> unionField(j, "string")))
-		)
+  def searchCallSetsRequest(jsonTxt: String): String = {
+    val json: JsValue = Json.parse(jsonTxt)
+    val fields = List(
+      ("name",     () => Json.obj("name" -> JsNull),   (j: JsValue) => Json.obj("name"  -> unionField(j, "string"))),
+      ("pageSize",     () => Json.obj("pageSize" -> JsNull),   (j: JsValue) => Json.obj("pageSize"  -> unionField(j, "int"))),
+      ("pageToken",    () => Json.obj("pageToken" -> JsNull),  (j: JsValue) => Json.obj("pageToken" -> unionField(j, "string")))
+    )
 
-		val sanJson = sanField(fields, json.as[JsObject])
-		sanJson.as[JsValue].toString()		
-	}
+    val sanJson = sanField(fields, json.as[JsObject])
+    sanJson.as[JsValue].toString()    
+  }
 
 }
